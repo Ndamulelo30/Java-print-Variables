d50310faf78c97ec6baf387a2ea246845a9ab1fd
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-16 13:41:26 +0200

    Add files via upload

---
+++ 6.JavaPrintVariables/src/main/java/com/mycompany/javaprintvariables/App.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaprintvariables;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        String firstName = "Tshifhiwa ";
+        String lastName = "Mulaudzi";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        
+        int x =5;
+        int y = 17;
+        System.out.println(x+y);//Print the values of x and y
+    }
+}
